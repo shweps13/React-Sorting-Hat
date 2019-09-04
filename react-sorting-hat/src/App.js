6b06ea388c6974cb0c5a73eec4543377a12cab4b
Heorhii Siburov <g.siburov@gmail.com>
2019-09-04 01:05:04 -0700

    styling

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './hogwarts.png';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Hogwarts</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          You will know from which house you came...
         </p>
+        <div className="Card">
+          <p>ololo</p>
+        </div>
       </div>
     );
   }
